810ad9846fb273e84838b7d75a3472ce1b92ab0b
MayuriR29 <mayuri.gr@gmail.com>
2018-07-25 17:00:40 +0800

    Added changes to list authors and added separate function to get books,author

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-
+const API_HOST = process.env.REACT_APP_BOOKS_API || "http://localhost:3000";
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      books: []
+      books: [],
+      authors: []
     };
   }
   render() {
@@ -17,14 +18,26 @@ class App extends Component {
       </div>
     );
   }
-  async componentDidMount() {
-    const url =
-      process.env.REACT_APP_BOOKS_API || "http://localhost:3000/books";
+  async getBooks() {
+    const url = `${API_HOST}/books`;
     const response = await fetch(url);
     const books = await response.json();
     this.setState({
       books: books
     });
+  }
+
+  async getAuthors() {
+    const url = `${API_HOST}/authors`;
+    const response = await fetch(url);
+    const authors = await response.json();
+    this.setState({
+      authors: authors
+    });
+  }
+  async componentDidMount() {
+    await this.getBooks();
+    await this.getAuthors();
   }
 }
 
