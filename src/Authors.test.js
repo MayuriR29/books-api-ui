9b4b9cc51306176d411081103d6c9027b499a7b7
MayuriR29 <mayuri.gr@gmail.com>
2018-07-27 16:28:52 +0800

    mocked fetch function for testing

--- src/Authors.test.js
+++ src/Authors.test.js
@@ -1,33 +1,38 @@
-import React from 'react';
-import Authors from './Authors';
-global.fetch = require('jest-fetch-mock');
+import React from "react";
+import Authors from "./Authors";
+global.fetch = require("jest-fetch-mock");
 import ShallowRenderer from "react-test-renderer/shallow";
 
-it('title is returned when render is called first ',()=>{
-    const renderer=new ShallowRenderer();
-    renderer.render(<Authors/>);
-    const output = renderer.getRenderOutput();
-    expect(output).toMatchSnapshot();
+it("title is returned when render is called first ", () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<Authors />);
+  const output = renderer.getRenderOutput();
+  expect(output).toMatchSnapshot();
 });
-it('test title and authorlist after component mounted',async ()=>{
-    fetch.mockResponseOnce(JSON.stringify([
-        {
-            name:"Paulo Coelho",
-            age:50,
-            _id:'cascwdf23213'
-        },
-        {
-            name:"Robin Sharma",
-            age:52,
-            _id:'cascwdqwwdf23213'
-        }
-    ]));
-    const renderer=new ShallowRenderer();
-    renderer.render(<Authors/>);
+it("test title and authorlist after component mounted", async () => {
+  //to convert to json object [{"name":"Paulo Coelho","age":50,"_id":"cascwdf23213"},
+  // {"name":"Robin Sharma","age":52,"_id":"cascwdqwwdf23213"}]'
+  fetch.mockResponseOnce(
+    JSON.stringify([
+      {
+        name: "Paulo Coelho",
+        age: 50,
+        _id: "cascwdf23213"
+      },
+      {
+        name: "Robin Sharma",
+        age: 52,
+        _id: "cascwdqwwdf23213"
+      }
+    ])
+  );
 
-    const instance=renderer.getMountedInstance();
-    await instance.componentDidMount();
+  const renderer = new ShallowRenderer();
+  renderer.render(<Authors />);
 
-    const output = renderer.getRenderOutput();
-    expect(output).toMatchSnapshot();
-})
+  const instance = renderer.getMountedInstance();
+  await instance.componentDidMount();
+
+  const output = renderer.getRenderOutput();
+  expect(output).toMatchSnapshot();
+});
